11f5fa41c60d6d01c1b62f67fb845ecef0cfe36a
Karnaj <yoan.geran@hotmail.fr>
2018-01-20 17:40:40 +0100

    Update sprite.h

--- sprite.h
+++ sprite.h
@@ -3,22 +3,25 @@
  
 #include <SDL2/SDL.h>
 
+
+typedef struct s_sprite s_sprite;
+
+
 struct s_sprite
 {
     SDL_Texture *texture;
-    int w, h;
+    unsigned int w, h;
 };
 
-typedef struct s_sprite s_sprite;
 
 s_sprite *load_sprite(char path[], SDL_Renderer *renderer, SDL_Color *key);
 s_sprite *free_sprite(s_sprite *sprite);
 void display_sprite(SDL_Renderer *renderer, s_sprite *sprite, int x, int y);
-void display_scaled_sprite(SDL_Renderer *renderer, s_sprite *sprite, int x, int y, int scale);
+void display_scaled_sprite(SDL_Renderer *renderer, s_sprite *sprite, int x, int y, double scale);
 void display_dscaled_sprite(SDL_Renderer *renderer, s_sprite *sprite, int x, int y, double wscale, double hscale);
 void set_background_sprite(SDL_Renderer *renderer, s_sprite *sprite);
 void display_cliped_sprite(SDL_Renderer *renderer, s_sprite *sprite, int x, int y, SDL_Rect *src);
-void display_scaled_cliped_sprite(SDL_Renderer *renderer, s_sprite *sprite, int x, int y, SDL_Rect *src, int scale);
+void display_scaled_cliped_sprite(SDL_Renderer *renderer, s_sprite *sprite, int x, int y, SDL_Rect *src, double scale);
 void display_dscaled_cliped_sprite(SDL_Renderer *renderer, s_sprite *sprite, int x, int y, SDL_Rect *src, double wscale, double hscale);
 
 #endif
